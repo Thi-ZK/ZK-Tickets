f38d43139185ffeeede85ce26b7860c03cdb3f0f
Thi-ZK <thiago.dominicheli@gmail.com>
2022-06-15 20:37:12 -0300

    Update Ticket Route Small Enhancement

--- routes/tickets/update.js
+++ routes/tickets/update.js
@@ -5,11 +5,19 @@ const TicketModel = require('../../models/ticket');
 
 var urlencodedParser = bodyParser.urlencoded({ limit: '10mb', extended: false });
 
-router.post('/single/:ticket_id', urlencodedParser, async (req, res) => {
+router.post('/single/messages/:ticket_id', urlencodedParser, async (req, res) => {
 	let ticket_id = req.params.ticket_id;
-	let data_to_be_updated = req.body.data_to_be_updated;
+	let new_message = {
+		message: req.body.message,
+		date: Date(),
+		message_owner: 1, // TO DO
+		message_owner_name: "Athalia Sieghart" // TO DO
+	}
+	let ticket = await TicketModel.find({id: ticket_id}).catch((error) => {res.end("Ticket Not Found")});
+	ticket[0].messages.push(new_message);
+	let messages_array_to_be_updated = ticket[0].messages;
 
-	await TicketModel.updateOne({id: ticket_id}, data_to_be_updated)
+	await TicketModel.updateOne({id: ticket_id}, {messages: messages_array_to_be_updated})
 	.catch(() => {res.end("Action Unsuccessful");});
 
 	res.end("Ticket Updated");
