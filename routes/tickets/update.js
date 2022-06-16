aa53320859578cf81732921ecff2cc0fa425fb60
Thi-ZK <thiago.dominicheli@gmail.com>
2022-06-16 14:40:51 -0300

    Adding Message Deletion In Ticket Update

--- routes/tickets/update.js
+++ routes/tickets/update.js
@@ -5,17 +5,36 @@ const TicketModel = require('../../models/ticket');
 
 var urlencodedParser = bodyParser.urlencoded({ limit: '10mb', extended: false });
 
-router.post('/single/messages/:ticket_id', urlencodedParser, async (req, res) => {
+router.post('/single/messages/set/:ticket_id', urlencodedParser, async (req, res) => {
 	let ticket_id = req.params.ticket_id;
+	let ticket = await TicketModel.find({id: ticket_id}).catch((error) => {return res.end("Ticket Not Found")});
 	let new_message = {
 		message: req.body.message,
 		date: Date(),
+		id: ticket[0].messages.length + 1,
 		message_owner: 1, // TO DO
 		message_owner_name: "Athalia Sieghart" // TO DO
 	}
-	let ticket = await TicketModel.find({id: ticket_id}).catch((error) => {res.end("Ticket Not Found")});
+
 	ticket[0].messages.push(new_message);
 	let messages_array_to_be_updated = ticket[0].messages;
+
+	await TicketModel.updateOne({id: ticket_id}, {messages: messages_array_to_be_updated})
+	.catch(() => {res.end("Action Unsuccessful");});
+
+	res.end("Ticket Updated");
+});
+
+router.post('/single/messages/delete/:ticket_id/:message_id', urlencodedParser, async (req, res) => {
+	let ticket_id = req.params.ticket_id;
+	let message_id = req.params.message_id;
+
+	let ticket = await TicketModel.find({id: ticket_id})
+	.catch((error) => {return res.end("Ticket Not Found")});
+
+	let messages_array_to_be_updated = ticket[0].messages.filter((msg) => {
+		return msg.id !== Number(message_id);
+	});
 
 	await TicketModel.updateOne({id: ticket_id}, {messages: messages_array_to_be_updated})
 	.catch(() => {res.end("Action Unsuccessful");});
