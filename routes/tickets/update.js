c7fce746a3ce3f1b820d8f103f6968a3ea154db8
Thi-ZK <thiago.dominicheli@gmail.com>
2022-06-26 13:37:20 -0300

    Updates Optimization

--- routes/tickets/update.js
+++ routes/tickets/update.js
@@ -8,46 +8,36 @@ var urlencodedParser = bodyParser.urlencoded({ limit: '10mb', extended: false })
 // Meant For Setting A New Message For A Single Ticket
 router.post('/single/messages/set/:ticket_id', urlencodedParser, async (req, res) => {
 	let ticket_id = req.params.ticket_id;
-	let ticket = await TicketModel.findOne({id: ticket_id}).select();
-
-	if (!ticket) {
-		return res.end("Ticket Not Found");
-	}
-
+	let req_status = "Ticket Updated";
 	let new_message = {
 		message: req.body.message,
 		date: new Date(),
 		date_casual_format: Date(),
-		id: ticket.messages.length + 1,
+		id: String(Date.now()) + "." + String(Math.floor(Math.random() * 100000)),
 		message_owner: 1, // TO DO
 		message_owner_name: "Athalia Sieghart", // TO DO
 		status: "alive"
 	}
 
-	ticket.messages.push(new_message);
-	let messages_array_to_be_updated = ticket.messages;
+	await TicketModel.updateOne({id: ticket_id}, {$push: {messages: new_message}})
+	.catch((error) => {req_status = "Ticket Doesn't Exist"});
 
-	await TicketModel.updateOne({id: ticket_id}, {messages: messages_array_to_be_updated})
-
-	res.end("Ticket Updated");
+	res.end(req_status);
 });
 
 // Meant For Deleting A Message For A Single Ticket
-router.post('/single/messages/delete/:ticket_id/:message_id', urlencodedParser, async (req, res) => {
+router.post('/single/messages/delete/:ticket_id/', urlencodedParser, async (req, res) => {
 	let ticket_id = req.params.ticket_id;
-	let message_id = req.params.message_id;
-	let ticket = await TicketModel.findOne({id: ticket_id}).select();
+	let message_id = req.body.message_id;
+	let req_status = "Ticket Updated";
 
-	if (!ticket) {
-		return res.end("Ticket Not Found");
-	}
+	await TicketModel.findOneAndUpdate({
+		id: ticket_id,
+		'messages.id': message_id
+	}, {'messages.$.status': "deleted"})
+	.catch(() => {req_status = "Ticket Doesn't Exist";});
 
-	let messages_array_to_be_updated = ticket.messages;
-	messages_array_to_be_updated[message_id - 1].status = "deleted";
-
-	await TicketModel.updateOne({id: ticket_id}, {messages: messages_array_to_be_updated})
-
-	res.end("Ticket Updated");
+	res.end(req_status);
 });
 
 // Meant For Setting A New Status For A Single Ticket
@@ -63,45 +53,27 @@ router.post('/single/status/:ticket_id', urlencodedParser, async (req, res) => {
 // Meant For Setting A New Assigned User For A Single Ticket
 router.post('/single/assigneds/set/:ticket_id', urlencodedParser, async (req, res) => {
 	let ticket_id = req.params.ticket_id;
-	let new_assigned_id = req.body.assignedId;
-	let new_assigned_name = req.body.assignedName;
-	let ticket = await TicketModel.findOne({id: ticket_id}).select();
+	let new_assumer = req.body.assignedId;
+	let new_assumer_name = req.body.assignedName;
+	let req_status = "Ticket Updated";
 
-	if (!ticket) {
-		return res.end("Ticket Not Found");
-	}
-
-	let new_assumers = ticket.assumers;
-	new_assumers.push(new_assigned_id);
-
-	let new_assumers_names = ticket.assumers_names;
-	new_assumers_names.push(new_assigned_name);
-
-	await TicketModel.updateOne({id: ticket_id}, {assumers: new_assumers, assumers_names: new_assumers_names})
+	await TicketModel.updateOne({id: ticket_id}, {$push: {assumers: new_assumer, assumers_names: new_assumer_name}})
+	.catch(() => {req_status = "Ticket Doesn't Exist"});
 	
-	res.end("Ticket Updated");
+	res.end(req_status);
 });
 
 // Meant For Deleting A Assigned User For A Single Ticket
 router.post('/single/assigneds/delete/:ticket_id', urlencodedParser, async (req, res) => {
 	let ticket_id = req.params.ticket_id;
-	let assigned_id = req.body.assignedId;
-	let assigned_name = req.body.assignedName;
-	let ticket = await TicketModel.findOne({id: ticket_id}).select();
+	let new_assumer = req.body.assignedId;
+	let new_assumer_name = req.body.assignedName;
+	let req_status = "Ticket Updated";
 
-	if (!ticket) {
-		return res.end("Ticket Not Found");
-	}
+	await TicketModel.updateOne({id: ticket_id}, {$pull: {assumers: new_assumer, assumers_names: new_assumer_name}})
+	.catch(() => {req_status = "Ticket Doesnt' Exist"});
 
-	let new_assumers = ticket.assumers
-	.filter((id) => {return id !== assigned_id});
-
-	let new_assumers_names = ticket.assumers_names
-	.filter((name) => {return name !== assigned_name});
-
-	await TicketModel.updateOne({id: ticket_id}, {assumers: new_assumers, assumers_names: new_assumers_names})
-	
-	res.end("Ticket Updated");
+	res.end(req_status);
 });
 
 module.exports = router;
