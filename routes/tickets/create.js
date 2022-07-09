a533829ca9da6f780876a3d16c3947374c5f4b82
Thi-ZK <thiago.dominicheli@gmail.com>
2022-07-08 23:43:27 -0300

    Minor Continuation Of Ticket Creation

--- routes/tickets/create.js
+++ routes/tickets/create.js
@@ -12,15 +12,15 @@ router.post('/single', urlencodedParser, async (req, res) => {
 	const newTicketDocument = new TicketModel({
 		name: ticket_data.name,
 		id: last_ticket[0].id + 1,
-		related_users: [], // TO DO
+		related_users: ticket_data.related_users,
 		related_users_names: ticket_data.related_users_names,
-		groups: [], // TO DO
+		groups: ticket_data.groups,
 		groups_names: ticket_data.groups_names,
 		description: ticket_data.description,
 		creator: req.session.user.id,
 		creator_name: req.session.user.name,
 		status: ticket_data.status,
-		assumers: [], // TO DO
+		assumers: ticket_data.assumers,
 		assumers_names: ticket_data.assumers_names,
 		creation_date: new Date(),
 		due_date: ticket_data.due_date,
