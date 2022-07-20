99ab608922b5388912354ab0500c4a9d214187c6
Thi-ZK <thiago.dominicheli@gmail.com>
2022-07-20 20:40:04 -0300

    Trying Solution For Set Cookie 7

--- server.js
+++ server.js
@@ -43,8 +43,7 @@ app.use(session({
     cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 1, // one day
         domain: process.env.SESSION_COOKIE_DOMAIN,
-        path: "/",
-        secure: true
+        path: "/"
     }
 }));
 
