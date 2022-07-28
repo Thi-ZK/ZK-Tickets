82bfe0d4f0359d3733bc21952a4d9fd93401ebd5
Thi-ZK <thiago.dominicheli@gmail.com>
2022-07-27 22:56:36 -0300

    Continue Implementation Of Profile

--- client/src/components/Pages/Profile/Preferences.js
+++ client/src/components/Pages/Profile/Preferences.js
@@ -1,9 +1,28 @@
 import React from 'react'
 
 function Preferences() {
-  return (
-    <div>meow</div>
-  )
+    return (
+    <div>
+        <div className="PFL-">
+            <div>
+                <input type="radio" name="rb" id="rb1" />
+                <label htmlFor="rb1">Español</label>
+            </div>
+            <div>
+                <input type="radio" name="rb" id="rb2" />
+                <label htmlFor="rb2">Deutsch</label>
+            </div>
+            <div>
+                <input type="radio" name="rb" id="rb3" />
+                <label htmlFor="rb3">English</label>
+            </div>
+            <div>
+                <input type="radio" name="rb" id="rb4" />
+                <label htmlFor="rb4">Português</label>
+            </div>
+        </div>
+    </div>
+    )
 }
 
 export default Preferences;
