5f2b3d64f197971170840192f159d1fe14ca2c8b
Thi-ZK <thiago.dominicheli@gmail.com>
2022-07-28 20:00:37 -0300

    Continue Implementation Of Profile Preferences

--- client/src/components/Pages/Profile/Preferences.js
+++ client/src/components/Pages/Profile/Preferences.js
@@ -1,24 +1,44 @@
 import React from 'react'
 
-function Preferences() {
+function Preferences () {
     return (
-    <div>
-        <div className="PFL-">
-            <div>
-                <input type="radio" name="rb" id="rb1" />
-                <label htmlFor="rb1">Español</label>
+    <div id="PFL-preferences-container" css-marker="PRE">
+        <div className="PFL-PRE-content-line-container">
+            <div id='PFL-PRE-languages-direct-container'>
+                <h3>Preferred Language</h3>
+                <div className='PFL-PRE-language-direct-container'>
+                    <input className="PFL-PRE-language-input" type="radio" name="pfl-language" id="PFL-PRE-spanish" />
+                    <label className="PFL-PRE-language-label" htmlFor="PFL-PRE-spanish">Español</label>
+                    <img src="./imgs/general/country_flag_icons/spain.png"/>
+                </div>
+                <div className='PFL-PRE-language-direct-container'>
+                    <input className="PFL-PRE-language-input" type="radio" name="pfl-language" id="PFL-PRE-german" />
+                    <label className="PFL-PRE-language-label" htmlFor="PFL-PRE-german">Deutsch</label>
+                    <img src="./imgs/general/country_flag_icons/germany.png"/>
+                </div>
+                <div className='PFL-PRE-language-direct-container'>
+                    <input className="PFL-PRE-language-input" type="radio" name="pfl-language" id="PFL-PRE-english" />
+                    <label className="PFL-PRE-language-label" htmlFor="PFL-PRE-english">English</label>
+                    <img src="./imgs/general/country_flag_icons/usa.png"/>
+                </div>
+                <div className='PFL-PRE-language-direct-container'>
+                    <input className="PFL-PRE-language-input" type="radio" name="pfl-language" id="PFL-PRE-portuguese" />
+                    <label className="PFL-PRE-language-label" htmlFor="PFL-PRE-portuguese">Português</label>
+                    <img src="./imgs/general/country_flag_icons/brazil.png"/>
+                </div>
             </div>
-            <div>
-                <input type="radio" name="rb" id="rb2" />
-                <label htmlFor="rb2">Deutsch</label>
-            </div>
-            <div>
-                <input type="radio" name="rb" id="rb3" />
-                <label htmlFor="rb3">English</label>
-            </div>
-            <div>
-                <input type="radio" name="rb" id="rb4" />
-                <label htmlFor="rb4">Português</label>
+            <div id='PFL-PRE-brightness-themes-direct-container'>
+                <h3>Preferred Brightness Theme</h3>
+                <div className='PFL-PRE-brightness-theme-direct-container'>
+                    <input className="PFL-PRE-brightness-theme-input" type="radio" name="pfl-brightness-theme" id="PFL-PRE-bright" />
+                    <label className="PFL-PRE-brightness-theme-label" htmlFor="PFL-PRE-bright">Bright</label>
+                    <img src="./imgs/headers/bright_sun.png"/>
+                </div>
+                <div className='PFL-PRE-brightness-theme-direct-container'>
+                    <input className="PFL-PRE-brightness-theme-input" type="radio" name="pfl-brightness-theme" id="PFL-PRE-dark" />
+                    <label className="PFL-PRE-brightness-theme-label" htmlFor="PFL-PRE-dark">Dark</label>
+                    <img src="./imgs/headers/dark_sun.png"/>
+                </div>
             </div>
         </div>
     </div>
--- client/src/components/Pages/Profile/UserInfo.js
+++ client/src/components/Pages/Profile/UserInfo.js
@@ -2,25 +2,25 @@ import React from 'react'
 
 function UserInfo ({ language, userData, texts }) {
     return (
-    <div id='PFL-person-info-container'>
-        <div className='PFL-person-info-direct-container'>
-            <p className='PFL-person-key-info'>{texts.user_id[language]}</p>
+    <div id='PFL-person-info-container' css-marker="PI">
+        <div className='PFL-PI-person-info-direct-container'>
+            <p className='PFL-PI-person-key-info'>{texts.user_id[language]}</p>
             <p>#{userData.id}</p>
         </div>
-        <div className='PFL-person-info-direct-container'>
-            <p className='PFL-person-key-info'>{texts.name[language]}</p>
+        <div className='PFL-PI-person-info-direct-container'>
+            <p className='PFL-PI-person-key-info'>{texts.name[language]}</p>
             <p>{userData.name}</p>
         </div>
-        <div className='PFL-person-info-direct-container'>
-            <p className='PFL-person-key-info'>Email</p>
+        <div className='PFL-PI-person-info-direct-container'>
+            <p className='PFL-PI-person-key-info'>Email</p>
             <p>{userData.email}</p>
         </div>
-        <div className='PFL-person-info-direct-container'>
-            <p className='PFL-person-key-info'>{texts.user_power[language]}</p>
+        <div className='PFL-PI-person-info-direct-container'>
+            <p className='PFL-PI-person-key-info'>{texts.user_power[language]}</p>
             <p>{userData.user_power === 4 ? userData.user_power + " - Admin (Max)" : userData.user_power}</p>
         </div>
-        <div className='PFL-person-info-direct-container'>
-            <p className='PFL-person-key-info'>{texts.phone_number[language]}</p>
+        <div className='PFL-PI-person-info-direct-container'>
+            <p className='PFL-PI-person-key-info'>{texts.phone_number[language]}</p>
             <p>{userData.phone}</p>
         </div>
     </div>
