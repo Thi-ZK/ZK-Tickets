3b66143fe641d2f26e4d86b865ae835374c82c4c
Thi-ZK <thiago.dominicheli@gmail.com>
2022-09-14 17:42:15 -0300

    Adding Smooth Effect Appearence For Mini Ticket Bands & Universal Overlay

--- client/src/components/Header/LeftHeader/MiniTicketBand.js
+++ client/src/components/Header/LeftHeader/MiniTicketBand.js
@@ -1,15 +1,26 @@
-import React           from 'react';
-import { useNavigate } from "react-router-dom";
-import texts           from '../../../languages/Header/MiniTicketBand.json';
+import React, { useState, useEffect } from 'react';
+import { useNavigate }                from "react-router-dom";
+import texts                          from '../../../languages/Header/MiniTicketBand.json';
 
 function MiniTicketBand({ ticket_data, language }) {
-    const navigate = useNavigate();
+    // Meant For When User Clicks In The Mini Ticket Band To Go To The Desired Ticket
+    const navigate           = useNavigate();
     const navigate_to_ticket = () => {
         navigate('/ticket_view/' + ticket_data.id);
     }
 
+    // Status Type (Homologation, Concluded...)
+    const status_type = ticket_data.status.toLowerCase();
+
+    // Meant For Smooth Appearence Effect Of Component Rendering
+    const [miniTicketStatus, updateMiniTicketStatus] = useState("off");
+
+    useEffect(() => {
+        updateMiniTicketStatus("on");
+    }, []);
+
     return (
-    <div onClick={navigate_to_ticket} className="LH-filtered-mini-ticket-band" css-marker="FTB" status={ticket_data.status.toLowerCase()}>
+    <div status={miniTicketStatus} onClick={navigate_to_ticket} className="LH-filtered-mini-ticket-band" css-marker="FTB" status-type={status_type}>
         <div className="LH-FTB-title-direct-container">
             <h3>{ticket_data.name}</h3>
             <p>ID: <span>#{ticket_data.id}</span></p>
